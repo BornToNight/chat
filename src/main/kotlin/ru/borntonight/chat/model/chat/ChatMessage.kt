bd27006ec7cd4b1175eeccc2114fac55587bfd90
vadim <you@example.com>
2023-11-10 16:09:03 +0500

    fix models

--- src/main/kotlin/ru/borntonight/chat/model/chat/ChatMessage.kt
+++ src/main/kotlin/ru/borntonight/chat/model/chat/ChatMessage.kt
@@ -8,13 +8,13 @@ import java.util.*
 @Document
 data class ChatMessage(
     @Id
-    var id: String = "",
+    var id: String = UUID.randomUUID().toString(),
     var chatId: String = "",
     var senderId: String = "",
     var recipientId: String = "",
     var senderName: String = "",
     var recipientName: String = "",
     var content: String = "",
-    var timestamp: Date,
-    var status: MessageStatus,
+    var timestamp: Date = Date(),
+    var status: MessageStatus = MessageStatus.DELIVERED,
 )
--- src/main/kotlin/ru/borntonight/chat/model/chat/ChatRoom.kt
+++ src/main/kotlin/ru/borntonight/chat/model/chat/ChatRoom.kt
@@ -2,11 +2,12 @@ package ru.borntonight.chat.model.chat
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.util.*
 
 @Document
 data class ChatRoom(
     @Id
-    val id: String = "",
+    val id: String = UUID.randomUUID().toString(),
     val chatId: String = "",
     val senderId: String = "",
     val senderName: String = "",
